380271009560e5d04bc56be65465cd3762421e53
DESKTOP-7K3OOIP\Frenk-PC <frenk-timmer@outlook.com>
2017-12-21 14:18:11 +0100

    Added presents tot fallingstate

--- phaser-es6-webpack/src/states/FallingState.js
+++ phaser-es6-webpack/src/states/FallingState.js
@@ -9,9 +9,21 @@ export default class extends Phaser.State {
   create () {
     const background = new Phaser.Sprite(game, 0, 0, 'chimney');
     this.game.add.existing(background);
-    this.sprite = game.add.sprite(game.world.centerX, -200, 'present-1');
-    this.sprite.anchor.set(0.5);
-    game.add.tween(this.sprite).to({ y: Config.height - this.sprite.centerY }, 2000, null, true)
+    var Present1 = game.add.sprite(Config.width / 2 - 100, -200, 'present-1');
+    var Present2 = game.add.sprite(Config.width / 2, -200, 'present-2');
+    var Present3 = game.add.sprite(Config.width / 2 + 100, -200, 'present-3');
+    var Present4 = game.add.sprite(Config.width / 2 + 150, -200, 'present-4');
+    var Present5 = game.add.sprite(Config.width / 2 - 150, -200, 'present-5');
+    Present1.anchor.set(0.5);
+    Present2.anchor.set(0.5);
+    Present3.anchor.set(0.5);
+    Present4.anchor.set(0.5);
+    Present5.anchor.set(0.5);
+    game.add.tween(Present1).to({ y: Config.height - Present1.centerY }, 2000, null, true);
+    game.add.tween(Present2).to({ y: Config.height - Present2.centerY }, 2000, null, true);
+    game.add.tween(Present3).to({ y: Config.height - Present3.centerY }, 2000, null, true);
+    game.add.tween(Present4).to({ y: Config.height - Present4.centerY }, 2000, null, true);
+    game.add.tween(Present5).to({ y: Config.height - Present5.centerY }, 2000, null, true);
   }
   render () {}
 }
--- phaser-es6-webpack/src/states/Splash.js
+++ phaser-es6-webpack/src/states/Splash.js
@@ -15,6 +15,10 @@ export default class extends Phaser.State {
     //
     this.load.image('bg', 'assets/images/background.jpg');
     this.load.image('present-1', 'assets/images/present-1.png');
+    this.load.image('present-2', 'assets/images/present-2.png');
+    this.load.image('present-3', 'assets/images/present-3.png');
+    this.load.image('present-4', 'assets/images/present-4.png');
+    this.load.image('present-5', 'assets/images/present-5.png');
     this.load.image('house', 'assets/images/house.png')
     this.load.image('chimney', 'assets/images/chimney.jpg')
 }
