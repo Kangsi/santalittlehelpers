4d028554ffb1115baa4c0d41bdcb785d7cd23d23
DESKTOP-7K3OOIP\Frenk-PC <frenk-timmer@outlook.com>
2017-12-21 10:33:56 +0100

    Added EndgameState and FallingState

--- phaser-es6-webpack/src/main.js
+++ phaser-es6-webpack/src/main.js
@@ -5,6 +5,8 @@ import Phaser from 'phaser'
 import BootState from './states/Boot'
 import SplashState from './states/Splash'
 import GameState from './states/Game'
+import FallingState from './states/FallingState'
+import EndgameState from './states/EndgameState'
 
 import config from './config'
 
@@ -12,9 +14,11 @@ class Game extends Phaser.Game {
   constructor () {
     super(config)
 
-    this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
-    this.state.add('Game', GameState, false)
+    this.state.add('Boot', BootState, false);
+    this.state.add('Splash', SplashState, false);
+    this.state.add('Game', GameState, false);
+    this.state.add('FallingAnimation', FallingState, false);
+    this.state.add('EndGame', EndgameState, false);
 
     // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
     if (!window.cordova) {
